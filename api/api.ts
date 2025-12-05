82ba0806db534d05ad3b6d4a89fbcc481ce63c23
Faraz Ghani <farazghani@Mac.lan>
2025-12-05 23:11:20 +0530

    updated bug

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "process.env.NEXT_PUBLIC_API_BASE_URL",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
